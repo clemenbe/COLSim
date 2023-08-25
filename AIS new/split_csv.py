84db872c1a11da943e5b17cbdc46baa651749614
mariedubro <133195749+mariedubro@users.noreply.github.com>
2023-08-25 15:58:53 +0930

    Update split_csv.py

--- AIS new/split_csv.py
+++ AIS new/split_csv.py
@@ -2,24 +2,24 @@ import pandas as pd
 import os
 
 def split_csv_by_mmsi(input_file):
-    # Charger le fichier CSV dans un DataFrame
+    # Charge the .csv file in a DataFrame
     df = pd.read_csv(input_file)
 
-    # Grouper les données par numéro MMSI
+    # Group the data by MMSI number
     grouped = df.groupby('sourcemmsi')
 
-    # Créer un dossier pour les fichiers CSV séparés
+    # Create a folder for the splited .csv files
     output_folder = 'csv_by_mmsi'
     os.makedirs(output_folder, exist_ok=True)
 
-    # Enregistrer les données groupées dans des fichiers CSV séparés
+    # Save the grouped data in the splited .csv files
     for mmsi, data in grouped:
         output_file = os.path.join(output_folder, f'{mmsi}.csv')
         data.to_csv(output_file, index=False)
 
-    print("Les fichiers CSV séparés ont été créés avec succès !")
+    print("Splited .csv file created with success !")
 
-# Appel de la fonction en spécifiant le chemin vers votre fichier CSV
-# input_file_path = 'chemin/vers/votre/fichier.csv'
+# Call of this function by specifying the path of the .csv file
+# input_file_path = 'path/of/your/file.csv'
 input_file_path = 'nari_dynamic.csv'
 split_csv_by_mmsi(input_file_path)
