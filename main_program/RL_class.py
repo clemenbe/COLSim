ada0e8e0e3dc49447bfe264de77454cd4ec17349
awanctin <antoine.wanctin@ensta-bretagne.org>
2024-05-01 16:56:28 +0930

    start of RL implementation, no reward and no action

---
+++ main_program/RL_class.py
@@ -0,0 +1,79 @@
+from gym import Env 
+from gym.spaces import Discrete, Box
+import numpy as np
+import random
+from stable_baselines3.common.env_checker import check_env
+from simulation_runner import SimulationRunner
+from calcul_tools import dist
+
+class ShowerEnv(Env):
+
+    def __init__(self):
+        # Actions that we can take: turn left, turn right, go straight
+        self.action_space = Discrete(3)
+        # Distance to objective and risk of collision
+        self.observation_space = Box(low=np.array([0, 0]), high=np.array([20, 1]), dtype=np.float64)
+        # Set start position of the Boat agent using initialize_sea_objects_random
+        runner = SimulationRunner()
+        self.boat = runner.initialize_sea_objects_random(["Boat"])[0]
+        # Set the destination of the Boat agent
+        destination_distance = 20
+        self.boat.phat = np.array([[self.boat.x + destination_distance * np.cos(self.boat.theta)], [self.boat.y + destination_distance * np.sin(self.boat.theta)]])
+        # Set the distance to the objective and the risk of collision
+        self.state = np.array([dist(np.array([[self.boat.x], [self.boat.y]]), self.boat.phat), 0], dtype=np.float64)
+        # Set the simulation lenght
+        self.simulation_length = 120
+
+
+
+
+    def step(self, action):
+        # Apply action
+        # 0 = turn left
+        # 1 = turn right
+        # 2 = go straight
+        # TODO: Implement action
+        self.state += action - 1
+
+
+        # Reduce shower length by 1 second
+        self.simulation_length -= 1
+
+        # TODO: Implement reward
+        # Calculate reward
+        if self.state >= 37 and self.state <= 39:
+            reward = 1
+        else:
+            reward = -1
+
+        # Check if simulation is done
+        if self.simulation_length <= 0:
+            done = True
+        else:
+            done = False
+
+        # Set placeholder for info
+        info = {}
+        return np.array([self.state], dtype=np.float64), reward, done, info
+
+    
+    def render(self):
+        pass
+    
+    def reset(self):
+        # Reset boat position and destination
+        runner = SimulationRunner()
+        self.boat = runner.initialize_sea_objects_random(["Boat"])[0]
+        # Set the destination of the Boat agent
+        destination_distance = 20
+        self.boat.phat = np.array([[self.boat.x + destination_distance * np.cos(self.boat.theta)], [self.boat.y + destination_distance * np.sin(self.boat.theta)]])
+        # Set the distance to the objective and the risk of collision
+        self.state = np.array([dist(np.array([[self.boat.x], [self.boat.y]]), self.boat.phat), 0], dtype=np.float64)
+
+        # Reset simulation time
+        self.simulation_length = 60
+        return np.array([self.state], dtype=np.float64)
+    
+
+env = ShowerEnv()
+check_env(env)
--- main_program/main.py
+++ main_program/main.py
@@ -3,4 +3,6 @@ from simulation_runner import SimulationRunner
 
 if __name__ == "__main__":
     runner = SimulationRunner()
+    runner.num_steps = 100
     runner.run()
+    
